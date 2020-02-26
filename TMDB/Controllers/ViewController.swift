4269a3b1bd303dca33049c0abb7d920fec5ac811
Bruno Fernandes <bruno.fernandesbfs@gmail.com>
2020-02-26 16:10:57 -0300

    Initial Project

---
+++ TMDB/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TMDB
+//
+//  Created by Bruno Fernandes on 26/02/20.
+//  Copyright © 2020 BFS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
